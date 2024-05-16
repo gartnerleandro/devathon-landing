e6b0f5792917ddba4bd3b18386c073385364ce97
Pedro Plasencia <programacion.en.esp@gmail.com>
2024-05-16 23:06:51 +0200

    feat: searchbar init base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://programacion-es.dev/',
   base: "./",
   server: {
     port: 3000
-  }
+  },
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
